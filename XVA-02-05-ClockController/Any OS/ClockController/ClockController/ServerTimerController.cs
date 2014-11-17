e9ecfa6cac5d3bec61230c78f580e78d8e995bf3
XSockets (@ulfbjo) <codeplanner@gmail.com>
2014-11-17 19:04:48 +0100

    More samples
    Added longrunning controller and updated simple messaging

---
+++ XVA-02-05-ClockController/Any OS/ClockController/ClockController/ServerTimerController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Timers;
+using XSockets.Core.XSocket;
+using XSockets.Core.XSocket.Helpers;
+using XSockets.Plugin.Framework;
+using XSockets.Plugin.Framework.Attributes;
+
+namespace ClockController
+{
+    /// <summary>
+    /// There will only be one instance of this class
+    /// A singleton...
+    /// </summary>
+    [XSocketMetadata("ServerTimer", PluginRange.Internal)]
+    public class ServerTimerController : XSocketController
+    {
+        public ServerTimerController()
+        {
+            var t = new Timer(1000);
+            t.Elapsed += (s, e) => this.InvokeToAll<ClockController>(DateTime.Now.ToString("hh:mm:ss"), "tick");
+            t.Start();
+        }
+    }
+}
