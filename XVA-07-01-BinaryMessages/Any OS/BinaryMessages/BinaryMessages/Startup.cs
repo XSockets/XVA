7c588ad8fa507d01cdf5061353a7b02070a7ae95
Magnus Thor <magnus@xsockets.net>
2014-11-18 09:33:54 +0100

    New exampe / episode
    BinaryMessage example

---
+++ XVA-07-01-BinaryMessages/Any OS/BinaryMessages/BinaryMessages/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using BinaryMessages;
+using XSockets.Core.Common.Socket;
+
+[assembly: PreApplicationStartMethod(typeof(Startup), "Start")]
+
+namespace BinaryMessages
+{
+    public static class Startup
+    {
+        private static IXSocketServerContainer _container;
+
+        public static void Start()
+        {
+            _container = XSockets.Plugin.Framework.Composable.GetExport<IXSocketServerContainer>();
+            _container.Start();
+        }
+    }
+}
---
+++ XVA-07-01-BinaryMessages/Win8+/BinaryMessages/BinaryMessages/Startup.cs
@@ -0,0 +1,17 @@
+﻿using BinaryMessages;
+using Microsoft.Owin;
+using Owin;
+using XSockets.Owin.Host;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace BinaryMessages
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseXSockets();
+        }
+    }
+}
