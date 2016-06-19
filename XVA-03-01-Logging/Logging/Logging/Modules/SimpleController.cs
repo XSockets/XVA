ff2d1a9c0e27ef48c2d2bdc2b19b815a67229368
Uffe Björklund <uffe@xsockets.net>
2016-06-19 20:23:23 +0200

    updated js and server

--- XVA-03-01-Logging/Logging/Logging/Modules/SimpleController.cs
+++ XVA-03-01-Logging/Logging/Logging/Modules/SimpleController.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Threading.Tasks;
 using XSockets.Core.Common.Utility.Logging;
 using XSockets.Core.XSocket;
+using XSockets.Core.XSocket.Helpers;
 using XSockets.Plugin.Framework;
 using XSockets.Plugin.Framework.Attributes;
 
@@ -9,16 +11,13 @@ namespace Logging.Modules
     [XSocketMetadata("Simple")]
     public class SimpleController : XSocketController
     {
-        /// <summary>
-        /// Log the incomming data and send it back to the caller only
-        /// </summary>
-        /// <param name="model"></param>
         public void LogTest(SomeModel model)
         {
             try
             {
                 //Get the logger and write with level information
-                Composable.GetExport<IXLogger>().Information("LogTest: {@model}", model); 
+                Composable.GetExport<IXLogger>().Information("LogTest: {@model}", model);                
+
                 //And... throw...
                 throw new Exception("Ohh crap!");
             }
