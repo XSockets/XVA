1b2e23abb39148016f1027cb7d9bec2fba5b8436
Uffe Björklund <uffe@xsockets.net>
2016-06-19 19:26:25 +0200

    updated to latest server and js

---
+++ XVA-01-03-SimpleRPC/SimpleRPC/packages/XSockets.ItemTemplates.5.0.2.1/tools/Install.ps1
@@ -0,0 +1,24 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$vsVersion = ""
+if($DTE.Version -eq "10.0"){$vsVersion = "2010"}
+if($DTE.Version -eq "11.0"){$vsVersion = "2012"}
+if($DTE.Version -eq "12.0"){$vsVersion = "2013"}
+if($DTE.Version -eq "14.0"){$vsVersion = "2015"}
+
+Try
+{
+    Write-Host "Installing XSockets item templates for visual studio $($vsVersion)" 
+
+    $itemTemplatesPathXSockets = [Environment]::GetFolderPath("MyDocuments") + "\Visual Studio "+$vsVersion+"\Templates\ItemTemplates\XSockets.NET 5"
+
+    if(!(Test-Path ($itemTemplatesPathXSockets))){New-Item ($itemTemplatesPathXSockets) -type directory}
+ 
+    Get-ChildItem $toolspath -Filter *.zip | ForEach{        
+        Copy-Item -Path $_.FullName -Destination $itemTemplatesPathXSockets        
+    }
+}
+Catch
+{
+    Write-Host "Could not copy XSockets templates to disc"
+}
