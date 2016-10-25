0d59bfe57cac75dd44fbee3b024dffd952b42c41
Uffe Björklund <uffe@xsockets.net>
2016-10-25 19:31:48 +0200

    updated to 5.6.7

--- XVA-03-05-ConfigurationBasic/ConfigurationBasic/ConfigurationBasic/Startup.cs
+++ XVA-03-05-ConfigurationBasic/ConfigurationBasic/ConfigurationBasic/Startup.cs
@@ -13,7 +13,8 @@ namespace ConfigurationBasic
         private static IXSocketServerContainer container;
         public static void Start()
         {
-            container = XSockets.Plugin.Framework.Composable.GetExport<IXSocketServerContainer>();
+            
+            container = XSockets.Plugin.Framework.Composable.GetExport<IXSocketServerContainer>();            
             container.Start();
 
             //// You can also add custom confgurations at startup.
